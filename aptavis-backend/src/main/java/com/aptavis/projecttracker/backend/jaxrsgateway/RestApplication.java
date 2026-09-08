3a059708364c941e4911aefb7f0305e8f4b83ad2
wafi <145336561+wafi-khn@users.noreply.github.com>
2026-09-09 05:36:47 +0700

    fix(backend): use public Render Postgres hostname for external connectivity

--- aptavis-backend/src/main/java/com/aptavis/projecttracker/backend/jaxrsgateway/RestApplication.java
+++ aptavis-backend/src/main/java/com/aptavis/projecttracker/backend/jaxrsgateway/RestApplication.java
@@ -11,12 +11,12 @@ import java.util.Set;
 @DataSourceDefinition(
     name = "java:app/datasources/ProjectTrackerDS",
     className = "org.postgresql.ds.PGSimpleDataSource",
-    serverName = "dpg-dag1l20n74is73c5227g-a",
+    serverName = "dpg-dag1l20n74is73c5227g-a.singapore-postgres.render.com",
     portNumber = 5432,
     databaseName = "projecttracker_ctuo",
     user = "wafi",
     password = "5Cu4AG8W66PDP4B4SkNS3xu8zDQ7kXI1",
-    url = "jdbc:postgresql://dpg-dag1l20n74is73c5227g-a:5432/projecttracker_ctuo"
+    url = "jdbc:postgresql://dpg-dag1l20n74is73c5227g-a.singapore-postgres.render.com:5432/projecttracker_ctuo"
 )
 @ApplicationPath("/rest")
 public class RestApplication extends Application {
