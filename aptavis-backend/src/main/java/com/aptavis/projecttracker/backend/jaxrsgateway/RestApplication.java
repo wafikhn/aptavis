e1f39f5578b6ea70f4c201e7622097f11db46d36
wafi <145336561+wafi-khn@users.noreply.github.com>
2026-09-08 21:49:13 +0700

    Add @DataSourceDefinition to RestApplication for WildFly automated database binding

--- aptavis-backend/src/main/java/com/aptavis/projecttracker/backend/jaxrsgateway/RestApplication.java
+++ aptavis-backend/src/main/java/com/aptavis/projecttracker/backend/jaxrsgateway/RestApplication.java
@@ -2,11 +2,19 @@ package com.aptavis.projecttracker.backend.jaxrsgateway;
 
 import com.aptavis.projecttracker.backend.application.ProjectApplication;
 import com.aptavis.projecttracker.backend.application.TaskApplication;
+import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 import java.util.Set;
 
+@DataSourceDefinition(
+    name = "java:jboss/datasources/ProjectTrackerDS",
+    className = "org.postgresql.ds.PGSimpleDataSource",
+    url = "jdbc:postgresql://${env.POSTGRES_HOST:dpg-dag1l20n74is73c5227g-a}:5432/${env.POSTGRES_DB:projecttracker}",
+    user = "${env.POSTGRES_USER:postgres}",
+    password = "${env.POSTGRES_PASSWORD:postgres}"
+)
 @ApplicationPath("/rest")
 public class RestApplication extends Application {
 
